7b37018bcff3a87e1701c23d5a66a9567a6801b9
guna3005 <pranithreddyguna01@gmail.com>
2024-02-04 20:02:20 -0500

    Added server, Axios, and ChartJS

--- server.js
+++ server.js
@@ -21,8 +21,26 @@ const budget = {
             title: 'Grocery',
             budget: 110
         },
+        {
+            title: 'Medical Expenses',
+            budget: 50
+        },
+        {
+            title: 'Car Insurance',
+            budget: 30
+        },
+        {
+            title: 'Shopping',
+            budget: 100
+        },
+        {
+            title: 'Health Insurance',
+            budget: 25
+        },
     ]
 };
+
+app.use('/',express.static('public'));
 
 
 app.get('/budget', (req, res) => {
