81ac1bdcbd7e9ab957f385bb2eb5334caef78a5d
guna3005 <pranithreddyguna01@gmail.com>
2024-02-04 20:03:23 -0500

    Loading data from JSON file

--- server.js
+++ server.js
@@ -1,52 +1,20 @@
 // Budget API
 
-const express = require('express');
-const cors = require('cors');
+const express = require("express");
+const cors = require("cors");
 const app = express();
 const port = 3000;
 
 app.use(cors());
 
-const budget = {
-    myBudget: [
-        {
-            title: 'Eat out',
-            budget: 25
-        },
-        {
-            title: 'Rent',
-            budget: 275
-        },
-        {
-            title: 'Grocery',
-            budget: 110
-        },
-        {
-            title: 'Medical Expenses',
-            budget: 50
-        },
-        {
-            title: 'Car Insurance',
-            budget: 30
-        },
-        {
-            title: 'Shopping',
-            budget: 100
-        },
-        {
-            title: 'Health Insurance',
-            budget: 25
-        },
-    ]
-};
+const bud = require("./budget.json");
 
-app.use('/',express.static('public'));
+app.use("/", express.static("public"));
 
-
-app.get('/budget', (req, res) => {
-    res.json(budget);
+app.get("/budget", (req, res) => {
+  res.json(bud);
 });
 
 app.listen(port, () => {
-    console.log(`API served at http://localhost:${port}`);
+  console.log(`API served at http://localhost:${port}`);
 });
